c8c5d5173c79712e1db462f0d2b87e8bf2e58d61
Teresiah_Mwangi <teresiahmwangi03@gmail.com>
2024-05-20 16:30:08 +0300

    webpage

---
+++ Script.js
@@ -0,0 +1,4 @@
+document.getElementById('search-button').addEventListener('click', function() {
+    let searchTerm = document.getElementById('search-input').value;
+    alert('You searched for: ' + searchTerm);
+});
