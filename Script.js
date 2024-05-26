8c2b3f89e2239b3319a74866b3fd4407f7380d35
Teresiah_Mwangi <teresiahmwangi03@gmail.com>
2024-05-26 22:39:29 +0300

    mycommit6

--- Script.js
+++ Script.js
@@ -1,4 +1,18 @@
-document.getElementById('search-button').addEventListener('click', function() {
-    let searchTerm = document.getElementById('search-input').value;
-    alert('You searched for: ' + searchTerm);
+const searchIcon = document.querySelector('.bi.bi-search'); // Select search icon
+const searchInput = document.getElementById('search-input');
+
+searchIcon.addEventListener('click', function() {
+  const searchTerm = searchInput.value;
+  if (searchTerm.trim() !== '') { // Check if search term is not empty
+    alert(`You searched for: ${searchTerm}`);
+  }
+});
+const dropdownToggle = document.querySelector('.dropdown-toggle');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+
+dropdownToggle.addEventListener('click', function(event) {
+  // Prevent default link behavior (following the href)
+  event.preventDefault();
+
+  dropdownMenu.classList.toggle('show'); // Toggle 'show' class
 });
